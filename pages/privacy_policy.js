765b91ce3056dfd6da8b02aac92583ab27ab28bf
unknown <mahinjaman07>
2024-07-01 19:13:56 +0600

    added terms and condition page

---
+++ pages/privacy_policy.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layouts from '../src/layouts/Layouts';
+import Head from 'next/head';
+
+const privacy_policy = () => {
+    return <Layouts pageTitle="Privacy Policy">
+    <Head>
+        <title>DetectiveSEO | Privacy Policy</title>
+
+    </Head>
+    <section className="blog-area p-t-130 p-b-130">
+        <div className="container">
+            
+        </div>
+    </section>
+</Layouts>
+};
+
+export default privacy_policy;
---
+++ pages/refound_policy.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layouts from '../src/layouts/Layouts';
+import Head from 'next/head';
+
+const refound_policy = () => {
+    return <Layouts pageTitle="Refound Policy">
+        <Head>
+            <title>DetectiveSEO | Refound Policy</title>
+
+        </Head>
+        <section className="blog-area p-t-130 p-b-130">
+            <div className="container">
+                
+            </div>
+        </section>
+    </Layouts>;
+};
+
+export default refound_policy;
